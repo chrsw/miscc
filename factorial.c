a185d45e2d5d3ea1072fb04057d192194a644806
Chris Williams <chrsw@users.noreply.github.com>
2022-12-22 18:06:01 -0500

    Add cmd line input to resursive factorial.

--- factorial.c
+++ factorial.c
@@ -1,16 +1,17 @@
 /* vim: ts=4 sw=4 et:
  * 
- * factorial.c	Compute the factorial (n!) of an integer.
- *          Loop version.
+ * factorial.c	
+ *      Compute the factorial (n!) of an integer.
+ *      Loop version.
  *
- *          The definition of the factorial function:
+ * Description:
+ *      The definition of the factorial function:
+ *      n! = n * (n - 1) * (n - 2) * (n - 3) * ... * 3 * 2 * 1
+ *      Number to factorial comes from the command line or a default
+ *      value if none is passed by command line argument.
  *
- * n! = n * (n - 1) * (n - 2) * (n - 3) * ... * 3 * 2 * 1
- *
- * Number to factorial comes from the command line or a default
- * value if none is passed by command line argument.
- *
- * Returns success.
+ * Returns:
+ *      Success.
  */
 
 #include <stdio.h>
--- rfactorial.c
+++ rfactorial.c
@@ -5,21 +5,31 @@
  *      Recursive version.
  *
  * Description:
- *          The definition of the factorial function:
- *          n! = n * (n - 1) * (n - 2) * (n - 3) * ... * 3 * 2 * 1
- *          Uses a static default value for n.
+ *      The definition of the factorial function:
+ *      n! = n * (n - 1) * (n - 2) * (n - 3) * ... * 3 * 2 * 1
+ *      Number to factorial comes from the command line or a default
+ *      value if none is passed by command line argument.
  *
  * Returns:
- *          Success.
+ *      Success.
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 unsigned int rfactorial(unsigned int n);
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-	unsigned int n = 7U; 
+	unsigned int n = 0U; 
+
+    if (argc == 2) {
+       n = atoi(argv[1]); 
+    }
+    else {
+	    n = 5U;
+    }
+
 
 	printf("%d! = ", n);
 
