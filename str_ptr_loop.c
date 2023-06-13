0a9ff617bf197232eee00542d3679760becb260f
Chris Williams <chrsw@users.noreply.github.com>
2023-06-13 19:44:22 -0400

    Switch from using NULL to static array size.

--- str_ptr_loop.c
+++ str_ptr_loop.c
@@ -1,7 +1,8 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
- * str_ptr_loop.c
- *      Loop through a list of strings using pointers.
+ * ptr_str_arr_loop.c
+ *      Loop through an array of strings using pointers. Simple
+ *      app demonstrating how these are used.
  *
  */
 
@@ -10,20 +11,32 @@
 int main(int argc, char *argv[]) {
 
     int i = 0;
-    char *ss[] = {
-        "string",
-        "pointer",
-        "loop",
+    int j = 0;
+
+    const char *ss[] = {
+        "pointers",
+        "strings",
+        "arrays",
+        "loops",
+        "and",
         "in",
-        "c",
-        NULL
+        "c"
     };
-    
-    printf("str_ptr_loop\n"); 
+
+    /* get the length of list of strings at compile time. */
+    const unsigned char str_cnt = (unsigned char)
+                                    (sizeof(ss)/sizeof(*ss));
 
     do {
         printf("%s\n", ss[i++]);
-    } while (ss[i]);
+    } while (i < str_cnt);
+
+    i = 0;
+
+    do {
+        for (j = 0; j < str_cnt - i; j++) printf(" ");        
+        printf("%s\n", ss[i++]);
+    } while (i < str_cnt);
     
     return 0;
 }
