488d3d9683c362df8447dd4f7bd8e27dda323d89
Chris Williams <chrsw@users.noreply.github.com>
2023-06-12 23:38:19 -0400

    Add C source and header templates.

---
+++ template.c
@@ -0,0 +1,24 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *      Vim modeline for consistent editor settings across files.
+ *
+ * exCH-#.c
+ *      A brief description of the file.
+ *
+ * Description:
+ *      Template for code and solutions to exercises in The C Programming
+ *      Language 2nd Ed. A longer description of the program goes here.
+ *
+ * Input:
+ *      Describe the expected input.
+ *
+ * Other info:
+ *      Returns, build, notes, etc. 
+ *
+ */
+
+//#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
