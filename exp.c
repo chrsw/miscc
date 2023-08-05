784fd7eb037553352a1c29eb43f1a0e21f1a3a63
Chris Williams <chrsw@users.noreply.github.com>
2023-08-04 23:53:54 -0400

    Add examples using math lib.

---
+++ exp.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+double a = 0.000;
+int main() {
+	printf("exp(%.3g) = %.3g\n", a,exp(a));
+	return 0;
+}
---
+++ sine.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+double a = 3.1415962/2;
+int main() {
+	printf("sin(%.3g) = %.3g\n", a,sin(a));
+	return 0;
+}
