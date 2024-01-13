ec4e51c81d067ad57a7d06f64e84fe57de36ae12
Chris Williams <chrsw@users.noreply.github.com>
2024-01-12 23:48:16 -0500

    Add comments to scanf example.

--- scanf_ex.c
+++ scanf_ex.c
@@ -1,9 +1,12 @@
-// scanf() example
-//
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ * 
+ * Simple scanf() example.
+ */
 
 #include <stdio.h>
 #include <string.h>
 
+/* scanf_ex:  get some input from keyboard and print it */
 int main(int argc, char *argv[])
 {
 	char s[40];
