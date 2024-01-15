b7996069aa0fcb48ef7e80b0762aaf6a51dbebe4
Chris Williams <chrsw@users.noreply.github.com>
2024-01-14 20:19:49 -0500

    scanf_ex: Demo multiple scanf() calls.

--- scanf_ex.c
+++ scanf_ex.c
@@ -12,11 +12,19 @@ int main(int argc, char *argv[])
 	char s[40];
 	int n;
 	int res;
+    int x, y;
 
 	res = scanf("%s %d", s, &n);
 	printf("entered: %s %d\n", s, n);
 
 	printf("length = %lu\n", strlen(s));
+
+    printf("enter a point...\n");
+    getchar();
+
+    res = scanf("x %d y %d", &x, &y);
+    printf("x = %d, y = %d\n", x, y);
+
 	return res;
 
 }
