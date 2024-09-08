8a1cd608f70cbe59080bb18258dd89a6d968c8fc
Chris Williams <chrsw@users.noreply.github.com>
2024-09-08 12:41:01 -0400

    Formatting cleanup

--- factorial.c
+++ factorial.c
@@ -1,6 +1,4 @@
-/* vim: ts=4 sw=4 et:
- * 
- * factorial.c	
+/* factorial.c	
  *      Compute the factorial (n!) of an integer.
  *      Loop version.
  *
@@ -9,9 +7,6 @@
  *      n! = n * (n - 1) * (n - 2) * (n - 3) * ... * 3 * 2 * 1
  *      Number to factorial comes from the command line or a default
  *      value if none is passed by command line argument.
- *
- * Returns:
- *      Success.
  */
 
 #include <stdio.h>
@@ -22,8 +17,7 @@ int main(int argc, char *argv[]) {
 	unsigned int n = 0U;         // Number to factorial
 	unsigned long nfact = 0U;    // Intermediate and result
 
-    // Grab number to factorial from cmd line or use
-    // default value.
+    // Grab number to factorial from command line or use default value.
     // This isn't safe, just for demonstration.
     if (argc == 2) {
        n = atoi(argv[1]); 
