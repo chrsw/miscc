916d9b8f0517573e7587e5869900dc86a2ad79b0
Chris Williams <chrsw@users.noreply.github.com>
2024-10-23 20:25:48 -0400

    Add example using basename on command executed

---
+++ basename_cmd_name.c
@@ -0,0 +1,31 @@
+// basename_cmd_name.c  print the executable name without path
+//
+// Linux only?
+//
+// $ gcc -o basename_cmd_name basename_cmd_name
+//
+// $ ./basename_cmd_name
+///////////////////////////////////////////////////////////////////////////
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <libgen.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char *path;
+
+    path = malloc(sizeof(argv[0]+1));
+
+    // basename() works on copy of path
+    strcpy(path, argv[0]);
+
+    printf("argv[0]: %s\n", argv[0]);
+    //printf("path: %s\n", path);
+    printf("basename(path): %s\n", basename(path));
+
+    return EXIT_SUCCESS;
+}
