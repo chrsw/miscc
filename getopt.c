83fb7d857ecb069a9cf66ec3c5697011ab6b5536
Chris Williams <chrsw@users.noreply.github.com>
2024-11-09 18:39:30 -0500

    getopt: add comments

--- getopt.c
+++ getopt.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
     int opt = ' ';
     const char *optstring = "welp";
 
+    // print every option passed on command line
     while ((opt = getopt(argc, argv, optstring)) > 0)
         printf("%c\n", opt);
 
