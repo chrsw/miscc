46a845184c70f95be605003d7340c85576af089d
Chris Williams <chrsw@users.noreply.github.com>
2024-11-09 18:29:46 -0500

    allocin: minor cleanups

--- allocin.c
+++ allocin.c
@@ -1,5 +1,5 @@
 // allocin:  allocate space for input.
-//           read file into memory with a pointer to each line.
+//           read standard input into memory with a pointer to each line.
 //           line pointers are stored in array so lines can be access
 //           with a simple array index.
 //
@@ -13,34 +13,36 @@
 // Optionally list files to read in on the command line.
 // 
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "allocin.h"
 
-int allocin(char ***p);
 
-char **ghap_ln;                     // modified by allocin()
-char buf[BUFSIZ];
+char **ghap_ln;                     // both modified by allocin()
+char buf[BUFSIZ]; 
+char *progname;                     // program name for error messages
 
 #ifdef ALLOCIN_MAIN
 int main(int argc, char *argv[]) {
 
     int lines = 0;
 
+    progname = argv[0];
     (void)argc;
-    (void)argv;
 
-    // read input
+    // read input, allocate heap space
     lines = allocin(&ghap_ln);
 
-    // print out the input
+    // print out the standard input line-wise, in order
     for (int i = 0; i < lines; i++)
         printf("%s", ghap_ln[i]);
-    // print in reverse
+    // then print in reverse
     for (int i = lines-1; i >= 0; i--)
         printf("%s", ghap_ln[i]);
 
+    // allocated heap data no longer neaded
     free(ghap_ln);
     return 0;
 }
@@ -50,7 +52,7 @@ int main(int argc, char *argv[]) {
 /* allocin: read standard input lines into dynamically allocated arrays 
  *
  *     buf: global buffer for temporary line storage while data is being read
- *       p:  pointer to start of arrays
+ *       p: pointer to start of arrays
  * returns: number of lines read in or, -1 for no lines or memory error
  *
  */
@@ -67,16 +69,16 @@ int allocin(char ***p) {
         char **hap_new = (char **)realloc(hap_ln, 
                                           (lines+1)*sizeof(char **));
         if (hap_new == NULL) {
-            fprintf(stderr, "%s: memory error\n", __func__);
+            fprintf(stderr, "%s: %s: memory error\n", progname, __func__);
             return -1;
         }
-        
         hap_ln = hap_new;
         hap_ln[lines] = (char *)malloc(sizeof(buf)+1);
         memcpy(hap_ln[lines], buf, sizeof(buf));
         lines++;
 	}
 
+    // set the pointer function argument to the allocated heap data
     *p = hap_ln;
     return lines;
 }
