eee2c485e6b6c9f292d71941046aa3cf473df930
Chris Williams <chrsw@users.noreply.github.com>
2024-11-08 19:58:30 -0500

    getopt: simple example using getopt()

---
+++ getopt.c
@@ -0,0 +1,19 @@
+// getopt:  simply use getopt()
+//
+// $ gcc -o getopt getopt.c
+
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+
+    int opt = ' ';
+    const char *optstring = "welp";
+
+    while ((opt = getopt(argc, argv, optstring)) > 0)
+        printf("%c\n", opt);
+
+    printf("\n");
+    return 0;
+}
